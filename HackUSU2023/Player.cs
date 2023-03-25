b567c50b8da8078c304d06cd9c60e461f3d3a177
Ian Adams <A02252812@aggies.usu.edu>
2023-03-25 02:26:00 -0600

    Got rudimentary movement working

--- HackUSU2023/Player.cs
+++ HackUSU2023/Player.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,12 +9,38 @@ namespace HackUSU2023
 {
     public class Player : Entity
     {
+        private Vector2 position;
+        private Vector2 direction;
+
+        private int velocity;
+
         public Player() 
         { 
             width = 64; 
             height = 64;
+            x = 400;
+            y = 400;
+            velocity = 2;
         }
 
-        
+        public void moveYUp(GameTime gameTime)
+        {
+            y -= (int)(velocity * gameTime.ElapsedGameTime.TotalMilliseconds);
+        }
+
+        public void moveYDown(GameTime gameTime)
+        {
+            y += (int)(velocity * gameTime.ElapsedGameTime.TotalMilliseconds);
+        }
+
+        public void moveXUp(GameTime gameTime)
+        {
+            x += (int)(velocity * gameTime.ElapsedGameTime.TotalMilliseconds);
+        }
+
+        public void moveXDown(GameTime gameTime)
+        {
+            x -= (int)(velocity * gameTime.ElapsedGameTime.TotalMilliseconds);
+        }
     }
 }
--- HackUSU2023/WorldState.cs
+++ HackUSU2023/WorldState.cs
@@ -11,15 +11,17 @@ namespace HackUSU2023
 {
     internal class WorldState : GameState
     {
+        private Player player;
         public override void draw(GameTime gameTime)
         {
             spriteBatch.DrawString(mainGame.MainFont, "World", new Vector2(100, 100), Color.White);
 
-            spriteBatch.Draw(mainGame.main_sprite_sheet, new Vector2(200, 200), Color.White);
+            spriteBatch.Draw(mainGame.main_sprite_sheet, new Rectangle(player.X, player.Y, 64, 64), Color.White);
         }
 
         public override bool update(GameTime gameTime)
         {
+            keyboardHandler.update(gameTime);
             if (Keyboard.GetState().IsKeyDown(Keys.Escape))
             {
                 return true;
@@ -29,7 +31,11 @@ namespace HackUSU2023
 
         public WorldState(GraphicsDeviceManager graphics, SpriteBatch spriteBatch, MainGame mainGame) : base(graphics, spriteBatch, mainGame)
         {
-            
+            player = new Player();
+            keyboardHandler.addEntry(new Content.KeyboardHandler.KeyEntry(Keys.W, player.moveYUp, false));
+            keyboardHandler.addEntry(new Content.KeyboardHandler.KeyEntry(Keys.S, player.moveYDown, false));
+            keyboardHandler.addEntry(new Content.KeyboardHandler.KeyEntry(Keys.A, player.moveXDown, false));
+            keyboardHandler.addEntry(new Content.KeyboardHandler.KeyEntry(Keys.D, player.moveXUp, false));
         }
     }
 }
